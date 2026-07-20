7d7604bb7af47de326be5451f96ab0cb0edf3b44
guoyueyao-ai <guoyueyao3@gmail.com>
2026-07-20 19:48:11 +0800

    Update homework2.py

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -1,16 +1,23 @@
-'''
+"""
 This homework defines one method, called "arithmetic".
-that method, type `help homework2.arithmetic`.
-'''
+For more information about that method, type `help homework2.arithmetic`.
+
+Modify this code so that it performs one of four possible functions,
+as specified in the following table:
+
+                    isinstance(x,str)      isinstance(x,float)
+isinstance(y,str)   return x+y             return str(x)+y
+isinstance(y,float) return x*int(y)        return x*y
+"""
 
 def arithmetic(x, y):
-    """
-    Modify this code so that it performs one of four possible functions, 
-    as specified in the following table:
-
-                        isinstance(x,str)  isinstance(x,float)
-    isinstance(y,str)   return x+y         return str(x)+y
-    isinstance(y,float) return x*int(y)    return x*y
-    """
-    if isinstance(x, str):    if isinstance(y, str):        return x + y    else:        return x * int(y)else:    if isinstance(y, str):        return str(x) + y    else:        return x * y
-
+    if isinstance(x, str):
+        if isinstance(y, str):
+            return x + y
+        else:
+            return x * int(y)
+    else:
+        if isinstance(y, str):
+            return str(x) + y
+        else:
+            return x * y
